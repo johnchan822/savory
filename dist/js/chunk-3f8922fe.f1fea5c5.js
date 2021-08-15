4af153ea11e7bd30a3da986580b85b7e824814cd
johnchan822 <45100631+johnchan822@users.noreply.github.com>
2021-08-15 23:47:03 +0800

    Delete dist directory

--- dist/js/chunk-3f8922fe.f1fea5c5.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-3f8922fe"],{9703:function(e,n,c){"use strict";c.r(n);var t=c("7a23"),u={class:"full"},s=Object(t["h"])("h1",null," 404 找不到此頁面 ",-1);function i(e,n){return Object(t["v"])(),Object(t["e"])("div",u,[s])}c("eb8c");const o={};o.render=i;n["default"]=o},eb8c:function(e,n,c){"use strict";c("ed00")},ed00:function(e,n,c){}}]);
-//# sourceMappingURL=chunk-3f8922fe.f1fea5c5.js.map
